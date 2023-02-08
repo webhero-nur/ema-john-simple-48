6206221a9e470a39b3a49ded69617bee439baafa
webhero-nur <nurealom522@gmail.com>
2023-02-08 12:15:22 +0600

    Style Product Cart, style debug and button position

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,7 +6,15 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="product" />
-            <p className='product-name'>{name}</p>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Rating: {ratings}</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
